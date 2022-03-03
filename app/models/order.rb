82c0a4a5154f5da5ad766e7b193ae337939b80f0
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-03-03 07:21:52 +0000

    [Add]アソシエーション　(order)1:N(order_details)

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   ##アソシエーション　(customer)1:N(orders)
   belong_to :customer
+  ##アソシエーション　(order)1:N(order_details)
+  has_many :order_details, dependent: :destroy
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,6 @@
 class OrderDetail < ApplicationRecord
   ##アソシエーション　(item)1:N(order_details)
   belong_to :item
+  ##アソシエーション　(order)1:N(order_details)
+  belong_to :order
 end
