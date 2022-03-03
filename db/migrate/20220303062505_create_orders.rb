7ed722afcd6244ae67460d4e527b11a64bb72b6f
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-03-03 06:40:22 +0000

    [Add]カラムに備考欄の内容をコメント

--- db/migrate/20220303062505_create_orders.rb
+++ db/migrate/20220303062505_create_orders.rb
@@ -9,13 +9,13 @@ class CreateOrders < ActiveRecord::Migration[6.1]
       t.string :address, null: false, default: ""
       ##配送先氏名
       t.string :name, null: false, default: ""
-      ##支払方法
+      ##支払方法 enumで管理, {0: クレジットカード, 1: 銀行振込}
       t.integer :payment_method, null: false, default: ""
       ##請求額
       t.integer :total_payment, null: false, default: "0"
       ##送料
       t.integer :postage, null: false, default: ""
-      ##受注ステータス
+      ##受注ステータス enumで管理, {0: 入金待ち, 1:入金確認, 2: 製作中, 3: 発送準備中, 4: 発送済み}
       t.integer :order_status, null: false, default: "0"
       
       t.timestamps
--- db/migrate/20220303063256_create_order_details.rb
+++ db/migrate/20220303063256_create_order_details.rb
@@ -9,7 +9,7 @@ class CreateOrderDetails < ActiveRecord::Migration[6.1]
       t.integer :price, null: false, default: ""
       ##数量
       t.integer :amount, null: false, default: ""
-      ##製作ステータス
+      ##製作ステータス enumで管理, {0: 製作不可, 1:製作待ち, 2: 製作中, 3: 製作完了}
       t.integer :making_status, null: false, default: "0"
       
       t.timestamps
