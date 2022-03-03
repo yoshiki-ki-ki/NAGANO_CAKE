58065b036154cf7d3131e7a42881e99f78917141
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-03-03 01:02:06 +0000

    [Add]管理者genresコントローラー作成（index,edit）

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
     get 'items/index'
     get 'items/new'
     get 'items/show'
