8c5b302bd89079e8bda79fa14c90c0aae6081bba
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-03-03 01:16:24 +0000

    [Add]顧客ordersコントローラー作成(new,complete,index,show)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   namespace :public do
+    get 'orders/new'
+    get 'orders/complete'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
     get 'cart_items/index'
   end
   namespace :public do
