0d69a1db7e6e269acdb6108d20fb824ca276e657
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-03-04 11:13:16 +0000

    [Update]管理者：商品詳細画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -7,8 +7,14 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+  end
+  
+  private
+  def item_params
+    params.require(:item).permit(:nema, :introduction, :price, :is_active)
   end
 end
