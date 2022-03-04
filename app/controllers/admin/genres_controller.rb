b3726d9b20bc2ef609f4c9d9ee0fd5e0f3ca176c
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-03-04 12:18:14 +0000

    [Update]管理者：ジャンル一覧・追加画面

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,21 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+  
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to '/admin/genres'
   end
 
   def edit
   end
+  
+  private
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,26 +3,21 @@ Rails.application.routes.draw do
   namespace :public do
     get 'addresses/index'
     get 'addresses/edit'
-  end
-  namespace :public do
+
     get 'orders/new'
     get 'orders/complete'
     get 'orders/index'
     get 'orders/show'
-  end
-  namespace :public do
+
     get 'cart_items/index'
-  end
-  namespace :public do
+
     get 'customers/show'
     get 'customers/edit'
     get 'customers/out_confirmation'
-  end
-  namespace :public do
+
     get 'items/index'
     get 'items/show'
-  end
-  namespace :public do
+
     get 'homes/top'
     get 'homes/about'
   end
@@ -30,23 +25,18 @@ Rails.application.routes.draw do
   ##管理者
   namespace :admin do
     get 'orders/show'
-  end
-  namespace :admin do
+ 
     get 'customers/index'
     get 'customers/show'
     get 'customers/edit'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
+
+    resources :genres, only: [:index, :create, :edit, :update]
+
     get '/items' => 'items#index'
     get 'items/new'
     get 'items/show'
     get 'items/edit'
-  end
-  namespace :admin do
+
     get '/' => 'homes#top'
   end
   # 顧客用
