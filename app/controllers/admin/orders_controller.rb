5d9d1a3b859bd7b871051d8574785ff4b4e88166
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-03-06 08:48:10 +0000

    [Add]管理者：注文履歴詳細画面作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,11 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.orde
+  end
+  
+  private
+  def order_params
+    params.require(:order).permit(:customer_id, :postal_code, :address, :name, :payment_method, :total_payment, :postage, :order_status)
   end
 end
