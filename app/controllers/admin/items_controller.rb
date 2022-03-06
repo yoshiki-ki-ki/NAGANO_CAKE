59e859f790d9f9b1d695a846d34e0f6c52277d58
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-03-06 05:51:09 +0000

    [Fix]管理者：商品一覧誤字修正

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -22,6 +22,6 @@ class Admin::ItemsController < ApplicationController
   
   private
   def item_params
-    params.require(:item).permit(:genre_id, :nema, :introduction, :price, :is_active, :image)
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active, :image)
   end
 end
