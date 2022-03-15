b597e920d097788250e3d01d67d094ff42ee01e3
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-03-15 00:25:36 +0000

    [Add]ordersにenum実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,7 @@ class Order < ApplicationRecord
   belongs_to :customer
   ##アソシエーション　(order)1:N(order_details)
   has_many :order_details, dependent: :destroy
+  
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum order_status: { awaiting_payment: 0, payment_confirmation: 1, under manufacture:2, preparing_to_ship:3, already_shipped:4 }
 end
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,11 @@ module NaganoCake
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    # 日本語化させるためのコード　enum
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
   end
 end
+
+
