c0e1c8a2cba014a6cb7bd1492ebba95426434874
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-03-15 02:19:04 +0000

    [Add]顧客：注文情報入力画面作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,13 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
+  end
+  
+  def order_confirmation
+    @order = Order.new(params)
+    @order.customer_id = current_customer.id
+    @order.save
+    redirect_to public_order_path
   end
 
   def complete
@@ -9,5 +17,10 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+  end
+  
+  private
+  def order_params
+      params.require(:order).permit(:postal_code, :address, :name, :payment_method, :total_payment, :order_status)
   end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,8 @@
 class Address < ApplicationRecord
   ##アソシエーション (customer)1:N(addressse)
   belongs_to :customer
+  
+  def address_display
+    '〒' + postal_code + ' ' + address + ' ' + name
+  end
 end
