ec729bf2cb182dbce428e140a9d5b6e1f9dfe9a5
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-03-19 08:07:52 +0000

    [Fix]cart_item → order_detail データ移動＆表示修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -16,6 +16,24 @@ class Public::OrdersController < ApplicationController
     @order = Order.new(order_params)
     @order.customer_id = current_customer.id
     @order.save
+    @cart_items = current_customer.cart_items.all
+      # カート内にある商品情報は複数あるため、一個ずつ処理していく
+      @cart_items.each do |cart_item|
+        # まず最初に取り出した一つの商品に関するテーブルをorder_detailsに作成する
+        order_detail = OrderDetail.new
+        # order_detailsが持つカラムにデータを代入していく
+        # 注文ID
+        order_detail.order_id = @order.id
+        # 商品ID
+        order_detail.item_id = cart_item.item_id
+        # 購入時価格（税込）
+        order_detail.price = cart_item.item.price
+        # 数量
+        order_detail.amount = cart_item.amount
+        # order_detailsにデータを保存
+        order_detail.save
+      end
+    @cart_items.destroy_all
     redirect_to public_orders_complete_path
   end
   
@@ -28,10 +46,10 @@ class Public::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @order_detail = @order.order_details.all
     @order.postage = 800
-    @total_price = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
+    @total_price = @order_detail.inject(0) { |sum, item| sum + item.subtotal }
     @total_payment = @order.postage + @total_price
-    @order_detail = @order.prder_details.all
   end
   
   private
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -6,4 +6,9 @@ class OrderDetail < ApplicationRecord
   
   enum making_status: { cannot_be_manufactured: 0, awaiting_manufacture: 1, under_manufacture: 2, completion_of_production: 3 }
   
+  ## 小計を求めるメソッド
+  def subtotal
+      item.with_tax_price * amount
+  end
+  
 end
