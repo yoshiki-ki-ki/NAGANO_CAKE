6f92dd3f9ba54ef2455a0fd1c4d66880464a0934
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-03-19 09:05:20 +0000

    [Fix]customer:カート商品の個数編集

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -10,12 +10,11 @@ class Public::CartItemsController < ApplicationController
     # 外部キーに値を入れなければデータの保存ができない
     @cart_item.customer_id = current_customer.id
     @cart_item.save
-    redirect_to public_items_path, notice: "カートに追加されました"
   end
   
   def update
     @cart_item = CartItem.find(params[:id])
-    @cart_item.update
+    @cart_item.update(cart_item_params)
     redirect_to public_cart_items_path(current_customer.id)
   end
   
