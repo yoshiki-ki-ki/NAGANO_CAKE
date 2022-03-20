58445957522ff41da4a897c835f37fd4e8e15b2b
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-03-20 06:47:12 +0000

    [Update]customer 商品一覧画面の表示調整

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,9 +1,11 @@
 class Public::ItemsController < ApplicationController
   def index
     @genres = Genre.all
-    # nameに何かが入っていれば「Genre.find(params[:name])」該当のジャンルを表示させる。
+    # nameに何かが入っていれば「Genre.find(params[:name]).items.all」該当のジャンルを表示させる。
     # nameに何も入っていなければItem.allすべての商品を表示
     @items = params[:name].present? ? Genre.find(params[:name]).items.page(params[:page]) : Item.page(params[:page])
+    @item_all = params[:name].present? ? Genre.find(params[:name]).items.all : Item.all
+    @genre_name = params[:name].present? ?  Genre.find(params[:name]).name : "商品"
   end
 
   def show
