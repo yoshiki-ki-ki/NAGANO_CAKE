d04c5701308b62bca2095fa72cf97bd902fc2c61
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-03-22 07:57:13 +0000

    [Update]admin test

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -6,11 +6,11 @@ class Admin::ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
   def create
     @item = Item.new(item_params)
     @item.save
-    redirect_to admin_items_path
+    redirect_to admin_item_path(@item.id)
   end
 
   def show
@@ -20,13 +20,13 @@ class Admin::ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
   end
-  
+
   def update
     @item = Item.find(params[:id])
     @item.update(item_params)
     redirect_to admin_item_path(@item.id)
   end
-  
+
   private
   def item_params
     params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active, :image)
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,9 +6,25 @@ class Admin::OrdersController < ApplicationController
     @total_price = @order_detail.inject(0) { |sum, item| sum + item.subtotal }
     @total_payment = @order.postage + @total_price
   end
-  
+
+  def orders_update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to admin_order_path
+  end
+
+  def production_update
+    @order_detail = @order.order_details.find(params[:id])
+    @order_detail.update(order_detail_params)
+    redirect_to admin_order_path
+  end
+
   private
   def order_params
     params.require(:order).permit(:customer_id, :postal_code, :address, :name, :payment_method, :total_payment, :postage, :order_status)
+  end
+
+  def order_detail_params
+    params.require(:order_detail).permit(:making_status)
   end
 end
