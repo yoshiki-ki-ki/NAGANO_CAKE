cbdf148a1f0f60226dc39961467a0a8de625c9a4
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-03-22 08:21:24 +0000

    [Update]customer:test

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -33,6 +33,10 @@ class Public::SessionsController < Devise::SessionsController
     end
   end
 
+  def after_sign_in_path_for(resource)
+    public_customer_path(current_customer.id)
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
