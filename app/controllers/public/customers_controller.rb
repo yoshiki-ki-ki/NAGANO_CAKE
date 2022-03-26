ddfe33a70eadbcaa2c174a0a6cf512a7d7e3d0c4
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-03-26 10:19:04 +0000

    [Fix]細かい修正

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,12 +1,12 @@
 class Public::CustomersController < ApplicationController
   def show
-    @address = Customer.find(current_customer.id)
+    @customer = Customer.find(current_customer.id)
   end
 
   def edit
     @customer = Customer.find(current_customer.id)
   end
-  
+
   def update
     @customer = Customer.find(current_customer.id)
     @customer.update(customer_params)
@@ -15,10 +15,17 @@ class Public::CustomersController < ApplicationController
 
   def out_confirmation
   end
-  
+
+  def out_update
+    @customer = Customer.find(current_customer.id)
+    @customer.update(is_active: true)
+    reset_session
+    redirect_to root_path
+  end
+
   private
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_active)
   end
-  
+
 end
