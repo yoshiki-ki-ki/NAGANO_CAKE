921be91cb9c57151ad8458e5369142d168fcc80c
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-03-26 11:11:19 +0000

    [Update]customer:商品詳細画面のデザイン

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -11,6 +11,9 @@ class Admin::CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
   end
   
+  def update
+  end
+  
   private
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_active)
